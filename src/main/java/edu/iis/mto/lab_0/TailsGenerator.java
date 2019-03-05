2be1c6004f675690f741b7d2ed22da448edbff04
maksymilian.regula <209444@edu.p.lodz.pl>
2019-03-05 18:33:43 +0100

    all test passed

--- src/main/java/edu/iis/mto/lab_0/TailsGenerator.java
+++ src/main/java/edu/iis/mto/lab_0/TailsGenerator.java
@@ -6,7 +6,16 @@ import java.util.List;
 public class TailsGenerator {
 
     public List<String> tails(String value) {
-        return new ArrayList<>(value.length());
+        if (value == null) {
+            return new ArrayList<>(0);
+        }
+        ArrayList<String> listOfTrimStr = new ArrayList<>();
+        listOfTrimStr.add(value);
+        for (int i = 0; i < value.length(); i++) {
+            listOfTrimStr.add(value.substring(i + 1, value.length()));
+        }
+        System.out.println(listOfTrimStr);
+        return listOfTrimStr;
     }
 
 }
